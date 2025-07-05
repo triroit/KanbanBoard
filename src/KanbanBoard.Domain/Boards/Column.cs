a776a27a693a2ca268a9653bc1bb844f748daaa5
Victoria Prudkaya <victoria.vlpr12@gmail.com>
2025-07-05 23:07:23 +0300

    add domain models

---
+++ src/KanbanBoard.Domain/Boards/Column.cs
@@ -0,0 +1,12 @@
+﻿namespace KanbanBoard.Domain.Boards
+{
+    public class Column
+    {
+        public Guid Id { get; set; }
+        public required string Title { get; set; }
+        public List<Guid> TaskIds { get; set; } = [];
+        public Guid BoardId { get; set; }
+        public int Order { get; set; }
+        public ColumnType ColumnType { get; set; } = ColumnType.Base;
+    }
+}
---
+++ src/KanbanBoard.Domain/Comments/Comment.cs
@@ -0,0 +1,12 @@
+﻿namespace KanbanBoard.Domain.Comments
+{
+    public class Comment
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public required string CommentText { get; set; }
+        public Guid TaskId { get; set; }
+        public Guid? ParentId { get; set; }
+        public DateTime CreationDate { get; set; }
+    }
+}
