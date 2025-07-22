37768544039eb916fd71deac0738f96894a9e762
Victoria Prudkaya <victoria.vlpr12@gmail.com>
2025-07-22 21:30:29 +0300

    added application layer, repository, service and create validator

---
+++ KanbanBoard/src/KanbanBoard.Application/Boards/IBoardService.cs
@@ -0,0 +1,9 @@
+﻿using KanbanBoard.Contracts.Boards;
+
+namespace KanbanBoard.Application.Boards
+{
+    public interface IBoardsService
+    {
+        Task<Guid> Create(CreateBoardDto createBoardDto, CancellationToken cancellationToken);
+    }
+}
---
+++ KanbanBoard/src/KanbanBoard.Application/DependencyInjection.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using KanbanBoard.Application.Boards;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace KanbanBoard.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services.AddValidatorsFromAssembly(typeof(DependencyInjection).Assembly);
+
+            services.AddScoped<IBoardsService, BoardsService>();
+
+            return services;
+        }
+    }
+}
